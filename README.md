ea2c31590161025ab419607acc397bd8f45af073
sivchari <shibuuuu5@gmail.com>
2021-09-19 00:49:38 +0900

    setup

---
+++ README.md
@@ -0,0 +1,22 @@
+## swapper
+
+欲しいデザインはsmuiから調べてコンテナに入ってinstall
+
+e.g.
+```console
+npm install --save-dev @smui/button
+```
+
+```svelte
+<script>
+  import Button from '@smui/button';
+</script>
+```
+
+詳しくは
+
+### GitHub
+https://github.com/hperrin/svelte-material-ui
+
+### docs
+https://sveltematerialui.com/demo/button/
---
+++ src/swapper/src/lib/Counter.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Button, { Label } from "@smui/button";
+
+  let count: number = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<div>
+  <Button color="secondary" on:click={increment}>
+    <Label>Default</Label>
+  </Button>
+  <Button color="secondary" on:click={increment} disabled>
+    <Label>Disabled</Label>
+  </Button>
+  <Button color="secondary" on:click={increment} ripple={false}>
+    <Label>No Ripple</Label>
+  </Button>
+  <Button color="secondary" on:click={increment} class="myClass">
+    <Label>With a Class</Label>
+  </Button>
+</div>
+<pre>Click: {count}</pre>
+
+<style>
+</style>
